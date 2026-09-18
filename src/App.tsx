afc439a8db659ac04d541a4edc70f4c09c7452a3
rubeyatuljannat24 <rubeyatuljannat06@gmail.com>
2026-09-18 22:32:42 +0600

    Navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import Banner from "./components/Banner"
-import Footer from "./components/Footer"
-import Nav from "./components/Nav"
+// import Banner from "./components/Banner"
+// import Footer from "./components/Footer"
+// import Nav from "./components/Nav"
 
 function App() {
  
 
   return (
     <>
-    <Nav/>
+    {/* <Nav/>
     <Banner/>
-    <Footer/>
+    <Footer/> */}
     </>
   )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,23 +1,23 @@
-import heroImg from "../assets/banner-stack.png"
+// import heroImg from "../assets/banner-stack.png"
 
-const Banner = () => {
-    return (
-        <div className="flex justify-between items-center container mx-auto ">
-        <div className="pl-10">
-        <h2 className="text-5xl font-extrabold text-[#0F172A]">Build Your Ideal <br /> <span className="bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent text-5xl font-extrabold">Development Stack </span></h2>   
-         <p className="text-[#475569] py-5 text-xl">Explore frontend, backend, database, and tooling options,<br />
-        compare them side by side, and put together the stack that fits your <br />
-        next project.</p>
-        <div className="flex gap-2 items-center">
-    <button className="btn bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 rounded-lg px-4 py-2">Explore Technologies</button>
-    <button className="btn text-[#374151] bg-white px-8 py-2 font-medium text-sm rounded-lg transition-colors border border-gray-300 ">Learn more</button>
-        </div>
-        </div>
-        <div>
-            <img src={heroImg} alt="" />
-        </div>
-        </div>
-    );
-};
+// const Banner = () => {
+//     return (
+//         <div className="flex justify-between items-center container mx-auto ">
+//         <div className="pl-10">
+//         <h2 className="text-5xl font-extrabold text-[#0F172A]">Build Your Ideal <br /> <span className="bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent text-5xl font-extrabold">Development Stack </span></h2>   
+//          <p className="text-[#475569] py-5 text-xl">Explore frontend, backend, database, and tooling options,<br />
+//         compare them side by side, and put together the stack that fits your <br />
+//         next project.</p>
+//         <div className="flex gap-2 items-center">
+//     <button className="btn bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 rounded-lg px-4 py-2">Explore Technologies</button>
+//     <button className="btn text-[#374151] bg-white px-8 py-2 font-medium text-sm rounded-lg transition-colors border border-gray-300 ">Learn more</button>
+//         </div>
+//         </div>
+//         <div>
+//             <img src={heroImg} alt="" />
+//         </div>
+//         </div>
+//     );
+// };
 
-export default Banner;
+// export default Banner;
